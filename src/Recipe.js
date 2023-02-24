02ff0e60088cae91ed3c81814229acade327512a
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-02-24 23:41:19 +0100

    changed recipe css

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,7 +1,6 @@
 import React from "react";
 import style from "./recipe.module.css";
 
-
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={style.recipe}>
